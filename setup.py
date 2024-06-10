3505d5951037fc1ae564527c3137878452236370
NarynKadiyala <vkadiyala@alliant.edu>
2024-06-10 05:34:04 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= '0.0.0',
+    author="Naryn Kadiyala",
+    author_email="vkadiyala@alliant.edu",
+    packages= find_packages(),
+    install_requires = []
+)
