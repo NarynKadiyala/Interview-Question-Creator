a065309b878497a73686afc75f5d8f6657c22396
NarynKadiyala <vkadiyala@alliant.edu>
2024-06-13 04:48:50 +0530

    Resolved merge conflicts

--- setup.py
+++ setup.py
@@ -3,13 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = "Generative AI Project",
     version= '0.0.0',
-<<<<<<< HEAD
     author="Naryn Kadiyala",
     author_email="vkadiyala@alliant.edu",
-=======
-    author="Boktiar Ahmed Bappy",
-    author_email="entbappy73@gmail.com",
->>>>>>> 02b958cfc0de13c89b812c7d10a37191a7051706
     packages= find_packages(),
     install_requires = []
 )
